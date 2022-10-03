59762241f3de2ad9a3078b5128612e9d75346e41
knakhat <knakhat@entiovi.com>
2022-10-03 18:13:43 +0530

    updated

---
+++ business/src/components/Home.js
@@ -0,0 +1,17 @@
+function Home() {
+    return (
+      <div className="App">
+        <header>Pocket</header>
+        <nav>
+            <li>FEATURES</li>
+            <li>PRICING</li>
+            <li>BUSINESS</li>
+            <li>SMART CARD</li>
+        </nav>
+        
+      </div>
+    );
+  }
+  
+  export default Home;
+  
